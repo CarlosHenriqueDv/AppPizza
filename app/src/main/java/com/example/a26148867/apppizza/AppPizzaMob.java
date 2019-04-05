b8cac307e3f1d2c28db8cf59759b926d1b048fa2
CARLOS HENRIQUE ARAUJO MENDONÇA <26148867@sempreunama.com.br>
2019-04-05 18:03:06 -0300

    Programação Mobile - AppPizzaria Primeiro Programa

---
+++ app/src/main/java/com/example/a26148867/apppizza/AppPizzaMob.java
@@ -0,0 +1,13 @@
+package com.example.a26148867.apppizza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppPizzaMob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_pizza_mob);
+    }
+}
